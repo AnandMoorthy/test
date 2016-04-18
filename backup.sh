4639c92a0bf3850253e0fe595c170197052197d8
Anand Moorthy <janandhamoorthy@gmail.com>
2016-04-18 16:03:12 +0530

    04-18-2016

--- backup.sh
+++ backup.sh
@@ -3,5 +3,5 @@ mysqldump -u root -panand consumerapp > /home/anand/Dev/kc/consumerapp.sql
 git add .
 NOW=$(date +"%m-%d-%Y")
 git commit -m $NOW
-git push https://anandmoorthy:anandarchugh1@https://github.com/AnandMoorthy/test.git --all
+#git push https://anandmoorthy:anandarchugh1@https://github.com/AnandMoorthy/test.git --all
 echo "Done"
